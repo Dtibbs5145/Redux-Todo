81c0cbafc9a0746686ab8b36c6992ceb1213be5b
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-04-08 19:10:03 -0400

    initial commit

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return {
+      todo: [],
+      value: '',
+      completed: false
+    }
+  }
+}
+
+export default App;
